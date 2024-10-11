edf540bfa4213a8db6155198d75877b0050b6b19
dgermani <dgermani@emeal.nttdata.com>
2024-10-11 12:45:30 +0200

    feat: added guard to control private router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { onAuthStateChanged } from 'firebase/auth';
+import { useFirebaseAuth } from 'vuefire'
+
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +23,7 @@ const router = createRouter({
       path: '/admin',
       name: 'admin',
       component: () => import('../views/admin/AdminLayout.vue'),
+      meta: { requiresAuth: true },
       children: [
         {
           path: '/admin/propiedades',
@@ -38,5 +44,35 @@ const router = createRouter({
     },
   ]
 })
+
+// Guard
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(url => url.meta.requiresAuth);
+  console.log(requiresAuth);
+  if (requiresAuth) {
+    try {
+      await authenticateUser()
+      next()
+    } catch (error) {
+      next({ name: 'login' })
+    }
+  } else {
+    next()
+  }
+})
+
+function authenticateUser() {
+  const auth = useFirebaseAuth();
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      unsubscribe();
+      if (user) {
+        resolve(user);
+      } else {
+        reject(new Error('User not authenticated'));
+      }
+    });
+  });
+}
 
 export default router
